80a00cbc0aea8a6a17ed3615fb631078b49c0917
Avery Yang <averyyang2674@gmail.com>
2022-12-12 18:41:36 -0600

    finish hero

--- app/page.js
+++ app/page.js
@@ -1,28 +1,28 @@
 import { Footer, Navbar } from '../components';
 import {
-    About,
-    Explore,
-    Feedback,
-    GetStarted,
-    Hero,
-    Insights,
-    WhatsNew,
-    World,
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
 } from '../sections';
 
 const Page = () => (
-    <div className="bg-primary-black overflow-hidden">
-        <Navbar />
-        <Hero />
-        <About />
-        <Explore />
-        <GetStarted />
-        <WhatsNew />
-        <World />
-        <Insights />
-        <Feedback />
-        <Footer />
-    </div>
+  <div className="bg-primary-black overflow-hidden">
+    <Navbar />
+    <Hero />
+    <About />
+    <Explore />
+    <GetStarted />
+    <WhatsNew />
+    <World />
+    <Insights />
+    <Feedback />
+    <Footer />
+  </div>
 );
 
 export default Page;
