c7b011131d60492274e3876a440bc6192741281d
Avery Yang <averyyang2674@gmail.com>
2022-12-12 20:23:28 -0600

    removed a bunch of extensions

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -5,14 +5,10 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { ExploreCard, TitleText, TypingText } from '../components';
 import { fadeIn, staggerContainer } from '../utils/motion';
-import {exploreWorlds} from '../constants';
+import { exploreWorlds } from '../constants';
 
 // https://www.youtube.com/watch?v=ugCN_gynFYw&ab_channel=JavaScriptMastery
 
-const Explore = () => (
-  <section>
-    Explore section
-  </section>
-);
+const Explore = () => <section>Explore section</section>;
 
 export default Explore;
