252bf8feb6d9d27c5102a9225534edd8950fc544
Avery Yang <averyyang2674@gmail.com>
2022-12-12 19:48:13 -0600

    add gradients to page; complete about

--- app/page.js
+++ app/page.js
@@ -14,14 +14,30 @@ const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
+    
+    <div className="relative">
     <About />
+    <div className="gradient-03 z-0"/>
     <Explore />
+    </div>
+    
+    <div className="relative">
     <GetStarted />
+    <div className="gradient-04 z-0"/>
     <WhatsNew />
+    </div>
+    
+    <div className="relative">
     <World />
+    <div className="gradient-03 z-0"/>
     <Insights />
+    </div>
+    
+    <div className="relative">
     <Feedback />
+    <div className="gradient-05 z-0"/>
     <Footer />
+    </div>
   </div>
 );
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => (
         <div id="lightsource" className="absolute w-[50%] inset-0 gradient-01" />
         <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
             <img src="/search.svg" alt="search" className='w-[24px] h-[24px] object-contain' />
-            <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>MetaVS</h2>
+            <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>MetaVersus</h2>
             <img
                 src="/menu.svg"
                 alt="menu"
