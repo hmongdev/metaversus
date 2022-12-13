3b4e81e165db1f998484cf7ecc28ed57986aa914
Avery Yang <averyyang2674@gmail.com>
2022-12-12 19:51:42 -0600

    started explore

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { ExploreCard, TitleText, TypingText } from '../components';
+import { fadeIn, staggerContainer } from '../utils/motion';
+import {exploreWorlds} from '../constants';
+
 const Explore = () => (
   <section>
     Explore section
